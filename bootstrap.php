bb7d05eb1ab231ed8ca512b993510436416248b9
sonypiay <pyscho30@gmail.com>
2025-07-28 15:10:43 +0700

    remove file .env

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '.env.development');
 $dotenv->load();
 
 require_once __DIR__ . '/AzureBlobStorage.php';
